bfe804e3e1981b90bac1ee4609d148e7279fd5ac
gangsararyop <gangsararyo@gmail.com>
2021-11-08 10:18:03 +0700

    AG-2

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-console.log('dari master')
-console.log('dari master')
-console.log('dari master')
-console.log('dari master')
-console.log('dari master')
+console.log("dari master");
+console.log("dari master");
+console.log("dari master");
+console.log("dari master");
+console.log("dari master");
+
+console.log("AG-2");
+console.log("AG-2");
+console.log("AG-2");
+console.log("AG-2");
